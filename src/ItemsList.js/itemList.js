99a2d0e4f1c224b351800e4cf6b7ebe685c5ed3e
Elvira <lozanoantalia6@gmail.com>
2022-01-29 13:33:46 -0500

    items

---
+++ src/ItemsList.js/itemList.js
@@ -0,0 +1,9 @@
+import './itemList.scss'
+
+export const ItemList = ({greeting}) => {
+    return(
+        <section className ="ItemList">
+            <h2> {greetin} </h2>
+        </section>
+    )
+}
